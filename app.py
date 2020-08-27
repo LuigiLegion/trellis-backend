2e09f403364bc987224f783397ce3a70740b56a0
Tal Luigi <luigi.t.agr@gmail.com>
2020-08-26 23:10:01 -0400

    feat: implemented a postgresql database connection in app.py utilizing flask-sqlalchemy and sqlalchemy-utils, complete with a database reset utility function and initial Board, List, and Card table models

--- app.py
+++ app.py
@@ -1,11 +1,59 @@
 # Imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import database_exists, drop_database, create_database
+
+from secrets import (
+    POSTGRESQL_URI,
+    POSTGRESQL_USERNAME,
+    POSTGRESQL_PASSWORD,
+    POSTGRESQL_DATABASE,
+)
 
 
 # Initializations
 # Initialize server
 app = Flask(__name__)
+# Configure database connection
+DATABASE_URI = f"postgresql+psycopg2://{POSTGRESQL_USERNAME}:{POSTGRESQL_PASSWORD}@{POSTGRESQL_URI}/{POSTGRESQL_DATABASE}"
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Suppress deprecation warning
+# Initialize database
+db = SQLAlchemy(app)
+# db.init_app(app)
+
+
+@app.cli.command("resetdb")
+def reset_db():
+    """ Destroys and creates database and tables. """
+
+    if database_exists(DATABASE_URI):
+        print("Dropping database...")
+        drop_database(DATABASE_URI)
+
+    if not database_exists(DATABASE_URI):
+        print("Creating database...")
+        create_database(DATABASE_URI)
+
+    print("Creating tables...")
+    db.create_all()
+    print("Tables created successfully!")
+
+
+# Models
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25), unique=True, nullable=False)
+
+
+class List(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=False, nullable=False)
 
 
 # Run server
