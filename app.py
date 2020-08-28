0fffcf707c3d1819644471ec20bb91c57e77727d
Tal Luigi <luigi.t.agr@gmail.com>
2020-08-27 21:00:44 -0400

    chore: reordered column properties and removed explicit property definitions where they can be implicit across database models in app.py

--- app.py
+++ app.py
@@ -43,17 +43,17 @@ def reset_db():
 # Models
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(25), unique=True, nullable=False)
+    title = db.Column(db.String(25), nullable=False, unique=True)
 
 
 class List(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), unique=True, nullable=False)
+    title = db.Column(db.String(50), nullable=False, unique=True)
 
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=False, nullable=False)
+    title = db.Column(db.String(250), nullable=False)
 
 
 # Run server
